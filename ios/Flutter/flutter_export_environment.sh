9846ae1f2d2b69435600c8cb142fbb1b6b4b0f9e
chengyunfei <595650661@qq.com>
2020-04-16 23:20:50 +0800

    完善

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/mac126/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/mac126/Desktop/peplelife"
-export "FLUTTER_TARGET=/Users/mac126/Desktop/peplelife/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/mac126/Desktop/flutterDemo/peplelife"
+export "FLUTTER_TARGET=/Users/mac126/Desktop/flutterDemo/peplelife/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/mac126/flutter/bin/cache/artifacts/engine/ios"
--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -62,7 +62,7 @@ class _HomePageState extends State<HomePage> {
                ),
                RaisedButton(
                  onPressed: (){
-
+                   _choiceAction();
                  },
                  child: Text('选择完毕'),
                ),
@@ -77,4 +77,33 @@ class _HomePageState extends State<HomePage> {
        ),
     );
   }
+
+  void _choiceAction(){
+    print('开始选择NIIT喜欢的类型----------');
+    if (typeController.text.toString() == '') {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(title: Text('美女类型不能为空'))
+      );
+    }else {
+      getHttp(typeController.text.toString()).then((val){
+        setState(() {
+          showText = val['data']['name'].toString();
+        });
+      });
+    }
+  }
+
+
+  Future getHttp(String TypeText) async {
+    try {
+      Response response;
+      var data = {'name': TypeText};
+      response = await Dio().get('https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian', queryParameters: data);
+      return response.data;
+    } catch (e) {
+      return print(e);
+    }
+  }
+
 }
